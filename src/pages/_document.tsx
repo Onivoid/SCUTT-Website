f5e829b089cdeabc6c2ba39f5fd687ff2a490f84
onivoid <skullyfox.yt@gmail.com>
2024-01-23 04:33:39 +0100

    metas

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { PrimeReactProvider } from 'primereact/api';
+import Icon from '@/resources/icon.png';
 
 
 export default function Document({
@@ -9,7 +10,11 @@ export default function Document({
 }) {
   return (
     <Html>
-      <Head />
+      <Head>
+        <title>SCUTT - Star Citizen Universal Translation Tool</title>
+        <link rel="icon" href="https://github.com/Onivoid/SCUTT-Website/blob/master/src/resources/icon.png" />
+        <meta name="theme-color" content="#111827" />
+      </Head>
         <body>
           <PrimeReactProvider>
             <Main />
